1117c65f982a0a57d027764fba5d7c7a5931c215
Nate Ricklin <nate@rickl.in>
2016-04-13 14:04:07 -0600

    unit tests for simple workflows
    fix

--- tests/unit/test_simpleworkflow.py
+++ tests/unit/test_simpleworkflow.py
@@ -1,10 +1,11 @@
 '''
 Unit tests for the Task class
 '''
-from gbdxtools.simpleworkflows import Task, Workflow, InvalidInputPort
+from gbdxtools.simpleworkflows import Task, Workflow, InvalidInputPort, WorkflowError
 from gbdxtools import Interface
 import vcr
 from auth_mock import get_mock_gbdx_session
+import re
 
 # How to use the mock_gbdx_session and vcr to create unit tests:
 # 1. Add a new test that is dependent upon actually hitting GBDX APIs.
@@ -29,6 +30,7 @@ def test_simpleworkflows_initialize_task():
     assert 'AOP_Strip_Processor' in aoptask.name
     assert 'AOP_Strip_Processor' == aoptask.type
 
+
 @vcr.use_cassette('tests/unit/cassettes/test_initialize_simpleworkflows_task_with_valid_inputs.yaml',filter_headers=['authorization'])
 def test_initialize_simpleworkflows_task_with_valid_inputs():
     a = gbdx.Task("AOP_Strip_Processor", data='dummy', enable_acomp=False, enable_pansharpen=False)
@@ -38,6 +40,14 @@ def test_initialize_simpleworkflows_task_with_valid_inputs():
     assert 'enable_acomp' in [p['name'] for p in a.input_data]
     assert 'enable_pansharpen' in [p['name'] for p in a.input_data]
 
+@vcr.use_cassette('tests/unit/cassettes/test_simpleworkflows_task_output_reference.yaml',filter_headers=['authorization'])
+def test_simpleworkflows_task_output_reference():
+    aoptask = gbdx.Task("AOP_Strip_Processor", data='dummy', enable_acomp=False, enable_pansharpen=False)
+    log = aoptask.get_output('log')
+    assert log.startswith('source:')
+    assert log.endswith(':log')
+    assert "AOP_Strip_Processor" in log
+
 @vcr.use_cassette('tests/unit/cassettes/test_initialize_simpleworkflows_task_with_invalid_inputs_fails.yaml',filter_headers=['authorization'])
 def test_initialize_simpleworkflows_task_with_invalid_inputs_fails():
     try:
@@ -45,6 +55,59 @@ def test_initialize_simpleworkflows_task_with_invalid_inputs_fails():
     except InvalidInputPort as e:
         pass
     else:
-        raise
+        raise Exception('Failed test')
 
-    
+@vcr.use_cassette('tests/unit/cassettes/test_create_simpleworkflow.yaml',filter_headers=['authorization'])
+def test_create_simpleworkflow():
+    aoptask = gbdx.Task("AOP_Strip_Processor", data='dummy', enable_acomp=False, enable_pansharpen=False)
+    s3task = gbdx.Task("StageDataToS3", data=aoptask.get_output('log'), destination='dummydestination')
+    workflow = gbdx.Workflow([ s3task, aoptask ])
+
+    assert isinstance(workflow, Workflow)
+    assert workflow.id is None
+    assert workflow.name is not None
+
+    try:
+        workflow.status
+    except WorkflowError as e:
+        pass
+    else:
+        raise Exception('failed test')
+
+    assert not workflow.complete
+
+@vcr.use_cassette('tests/unit/cassettes/test_simpleworkflow_empty_tasks.yaml',filter_headers=['authorization'])
+def test_simpleworkflow_empty_tasks():
+    workflow = gbdx.Workflow([])
+
+    try:
+        workflow.execute()
+    except WorkflowError as e:
+        pass
+    else:
+        raise Exception('failed test')
+
+@vcr.use_cassette('tests/unit/cassettes/test_simpleworkflow_completed_status.yaml',record_mode='new_episodes',filter_headers=['authorization'])
+def test_simpleworkflow_completed_status():
+    workflow = gbdx.Workflow([])
+
+    workflow.id = '4309162525177689381'  # a known completed workflow
+    assert workflow.complete
+    assert not workflow.failed
+    assert not workflow.canceled
+    assert not workflow.succeeded
+    assert not workflow.running
+    assert workflow.timedout  # this particular workflow timed out
+
+
+
+
+
+
+
+
+
+
+
+
+
