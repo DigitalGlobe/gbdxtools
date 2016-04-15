d53b2d7d1a94549e9eef456b34bbf7321a3f15bf
Nate Ricklin <nate@rickl.in>
2016-04-14 19:14:25 -0600

    Bump version: 0.1.6 → 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info > (3,):
 # long_description=readme,
       
 setup(name='gbdxtools',
-      version='0.1.6',
+      version='0.2.0',
       description='Refactored to separate ordering, workflows, s3, idaho. New idaho functionality added.',
       classifiers=[],
       keywords='',
