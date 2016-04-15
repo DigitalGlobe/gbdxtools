dcf2b3cf6f570987156d6f42561a72f0e02b8b2a
nricklin <nate@rickl.in>
2016-04-15 13:47:48 -0600

    Update launch_simple_workflow.py

--- examples/launch_simple_workflow.py
+++ examples/launch_simple_workflow.py
@@ -2,7 +2,7 @@ from gbdxtools import Interface
 gbdx = Interface()
 
 data = "s3://receiving-dgcs-tdgplatform-com/054813633050_01_003" # WV02 Image over San Francisco
-aoptask = gbdx.Task("AOP_Strip_Processor", data=data, enable_acomp=False, enable_pansharpen=False)
+aoptask = gbdx.Task("AOP_Strip_Processor", data=data, enable_acomp=True, enable_pansharpen=True)
 workflow = gbdx.Workflow([ aoptask ]) 
 workflow.savedata(aoptask.outputs.data, location='some_folder')
 workflow.execute()
