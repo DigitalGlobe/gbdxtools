8f1336a17dc1be047adf576660c279849698d35f
Kostas Stamatiou <kostastamatiou@gmail.com>
2016-05-17 00:21:28 -0600

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info > (3,):
 # long_description=readme,
       
 setup(name='gbdxtools',
-      version='0.2.5',
+      version='0.2.6',
       description='Additional s3 functionality.',
       classifiers=[],
       keywords='',
