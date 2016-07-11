d5a3dfd389298316441857f1d103091eb12d87e5
brett55 <bjcull2@gmail.com>
2016-07-11 11:22:59 -0600

    Fix unit test

--- tests/unit/test_simpleworkflow.py
+++ tests/unit/test_simpleworkflow.py
@@ -286,12 +286,11 @@ class SimpleWorkflowTests(unittest.TestCase):
         submit a workflow with multiple params, hit batch workflow endpoint
         :return:
         """
-        gbdx = Interface()
         # note there are 2 inputs
         data = ["s3://receiving-dgcs-tdgplatform-com/054813633050_01_003",
                 "http://test-tdgplatform-com/data/QB02/LV1B/053702625010_01_004/053702625010_01/053702625010_01_P013_MUL"]
-        aoptask = gbdx.Task("AOP_Strip_Processor", data=data, enable_acomp=True, enable_pansharpen=True)
-        workflow = gbdx.Workflow([aoptask])
+        aoptask = self.gbdx.Task("AOP_Strip_Processor", data=data, enable_acomp=True, enable_pansharpen=True)
+        workflow = self.gbdx.Workflow([aoptask])
         workflow.savedata(aoptask.outputs.data, location='some_folder')
         batch_workflow_id = workflow.execute()
 
