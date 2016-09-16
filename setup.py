0e195edbbc4c6563f9462e934ebbdb92e227da1a
Nate Ricklin <nate@rickl.in>
2016-09-16 10:33:43 -0600

    Bump version: 0.5.4 → 0.5.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info > (3,):
 # long_description=readme,
       
 setup(name='gbdxtools',
-      version='0.5.4',
+      version='0.5.5',
       description='Additional s3 functionality.',
       classifiers=[],
       keywords='',
