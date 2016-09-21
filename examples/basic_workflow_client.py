f37d76995637f49ac06193d1fbee8ed651042907
dmitryzv <dmitry.zviagintsev@digitalglobe.com>
2016-09-21 13:35:42 -0600

    Feature task registry (#61)
    * Added task registry class.
    
    * Updated unit tests.
    
    * Incorporated comments, added functionality to register task from a kson file, flattened result of list tasks.

--- examples/basic_workflow_client.py
+++ examples/basic_workflow_client.py
@@ -3,8 +3,8 @@ from gbdxtools import Interface
 gbdx = None
 
 def go():
-    print gbdx.workflow.list_tasks()
-    print gbdx.workflow.describe_task('HelloGBDX')
+    print gbdx.task_registry.list()
+    print gbdx.task_registry.get_definition('HelloGBDX')
 
 if __name__ == "__main__":
     gbdx = Interface()
