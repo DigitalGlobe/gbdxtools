46a41b5561332836e324b7c7ec1145ee15a03b08
Nikki Aldeborgh <haaldeborgh@gmail.com>
2016-10-17 13:49:37 -0600

    Download s3 maintains directory structure (#67)
    * download maintains directory structure
    
    * test s3 download
    
    * replace token
    
    * update test s3 download
    
    * formatting
    
    * uncomment test

--- tests/unit/test_s3.py
+++ tests/unit/test_s3.py
@@ -2,6 +2,8 @@ from gbdxtools import Interface
 from gbdxtools.s3 import S3
 from auth_mock import get_mock_gbdx_session
 import vcr
+import os
+import tempfile
 import unittest
 
 """
@@ -16,13 +18,19 @@ How to use the mock_gbdx_session and vcr to create unit tests:
 6. Edit the cassette to remove any possibly sensitive information (s3 creds for example)
 """
 
+cassette_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cassettes', 'test_s3_download.yaml')
 
 class S3Tests(unittest.TestCase):
+
+    _temp_path = None
+
     @classmethod
     def setUpClass(cls):
         # create mock session, replace dummytoken with real token to create cassette
         mock_gbdx_session = get_mock_gbdx_session(token="dummytoken")
         cls.gbdx = Interface(gbdx_connection=mock_gbdx_session)
+        cls._temp_path = tempfile.mkdtemp()
+        print("Created: {}".format(cls._temp_path))
 
     def test_bucket_init(self):
         s = S3(self.gbdx)
@@ -37,3 +45,12 @@ class S3Tests(unittest.TestCase):
         assert "S3_secret_key" in s.info.keys()
         assert "S3_access_key" in s.info.keys()
         assert "S3_session_token" in s.info.keys()
+
+    @vcr.use_cassette(cassette_name, filter_headers=['authorization'])
+    def test_download(self):
+        location = 'nikki/gbdxtools_test_s3'
+        s = S3(self.gbdx)
+        s.download(location, local_dir=self._temp_path)
+
+        assert os.path.isfile(os.path.join(self._temp_path, 'test_dir', 'model.json'))
+        assert os.path.isfile(os.path.join(self._temp_path, 'model.json'))
