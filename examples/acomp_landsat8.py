6028394f2651730cde3b676db28964ff32bc42f3
Nate Ricklin <nate@rickl.in>
2017-01-18 13:52:50 -0700

    fix acomp landsat8 example script for new task name

--- examples/acomp_landsat8.py
+++ examples/acomp_landsat8.py
@@ -2,7 +2,7 @@
 from gbdxtools import Interface
 gbdx = Interface()
 
-acomp = gbdx.Task('AComp_New', data='s3://landsat-pds/L8/033/032/LC80330322015035LGN00')
+acomp = gbdx.Task('AComp', data='s3://landsat-pds/L8/033/032/LC80330322015035LGN00')
 workflow = gbdx.Workflow([acomp])
 workflow.savedata(acomp.outputs.data, location='acomp_output_folder')
 workflow.execute()
