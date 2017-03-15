cb6acf6748954acdfbddf111a53cde4a8cb5ee31
michaelconnor00 <mike@sparkgeo.com>
2017-03-15 14:37:57 -0700

    Add task update function and test.

--- gbdxtools/task_registry.py
+++ gbdxtools/task_registry.py
@@ -82,3 +82,18 @@ class TaskRegistry(object):
         r.raise_for_status()
 
         return r.text
+
+    def update(self, task_name, task_json):
+        """Updates a GBDX task.
+
+        Args:
+            task_name (str): Task name.
+            task_json (dict): Dictionary representing updated task definition.
+
+        Returns:
+            Dictionary representing the updated task definition.
+        """
+        r = self.gbdx_connection.put(self._base_url + '/' + task_name, json=task_json)
+        r.raise_for_status()
+
+        return r.json()
