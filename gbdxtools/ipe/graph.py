2feba152911f8d4f5136bfe10bad41e1b6a09d14
Chris Helm <christopher.helm@gmail.com>
2017-03-17 21:10:50 -0600

    passing auth via headers a bit more manually

--- gbdxtools/ipe/graph.py
+++ gbdxtools/ipe/graph.py
@@ -1,16 +1,18 @@
 import json
 import requests
 
-#VIRTUAL_IPE_URL = "http://virtualidaho-env.us-east-1.elasticbeanstalk.com/v1"
 VIRTUAL_IPE_URL = "http://idaho-api-lb-1256760776.us-east-1.elb.amazonaws.com/v1"
 
 def register_ipe_graph(conn, ipe_graph):
     url = "{}/graph".format(VIRTUAL_IPE_URL)
-    return requests.post(url, json.dumps(ipe_graph), headers={"Content-Type": "application/json"}).text
+    token = conn.access_token
+    return requests.post(url, json.dumps(ipe_graph), headers={"Content-Type": "application/json", "Authorization":"Bearer " + token}).text
 
 
 def get_ipe_metadata(conn, ipe_id, node='toa_reflectance'):
     meta = {}
-    meta['image'] = requests.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node)).json()
-    meta['georef'] = requests.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node)).json()
+    token = conn.access_token
+    headers={"Authorization":"Bearer " + token}
+    meta['image'] = requests.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node), headers=headers).json()
+    meta['georef'] = requests.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node), headers=headers).json()
     return meta
