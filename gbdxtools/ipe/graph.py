5aeb3c34fc89ffb695251b64aeb571613c540bf7
Chris Helm <christopher.helm@gmail.com>
2017-03-21 09:08:35 -0600

    new test api working with auth

--- gbdxtools/ipe/graph.py
+++ gbdxtools/ipe/graph.py
@@ -1,18 +1,14 @@
 import json
 import requests
 
-VIRTUAL_IPE_URL = "http://idaho-api-lb-1256760776.us-east-1.elb.amazonaws.com/v1"
+VIRTUAL_IPE_URL = "https://idahoapitest.geobigdata.io/v1"
 
 def register_ipe_graph(conn, ipe_graph):
     url = "{}/graph".format(VIRTUAL_IPE_URL)
-    token = conn.access_token
-    return requests.post(url, json.dumps(ipe_graph), headers={"Content-Type": "application/json", "Authorization":"Bearer " + token}).text
-
+    return conn.post(url, json.dumps(ipe_graph), headers={"Content-Type": "application/json"}).text
 
 def get_ipe_metadata(conn, ipe_id, node='toa_reflectance'):
     meta = {}
-    token = conn.access_token
-    headers={"Authorization":"Bearer " + token}
-    meta['image'] = requests.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node), headers=headers).json()
-    meta['georef'] = requests.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node), headers=headers).json()
+    meta['image'] = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node)).json()
+    meta['georef'] = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node)).json()
     return meta
