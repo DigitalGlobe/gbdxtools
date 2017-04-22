47d1f1589b1fe51d35bfb91788cf0be197ba6e02
Pramukta Kumar <pramukta.kumar@gmail.com>
2017-04-22 10:55:23 -0600

    modify graph REST helpers to work with both a Session and a FuturesSession

--- gbdxtools/ipe/graph.py
+++ gbdxtools/ipe/graph.py
@@ -1,25 +1,34 @@
 import json
-import requests
+from concurrent.futures import Future, wait
 
 VIRTUAL_IPE_URL = "https://idahoapi.geobigdata.io/v1"
 
 from gbdxtools.ipe.error import NotFound, BadRequest
 
+def resolve_if_future(future):
+    if isinstance(future, Future):
+        return future.result()
+    else:
+        return future
+
+
 def get_ipe_graph(conn, graph_id):
     url = "{}/graph/{}".format(VIRTUAL_IPE_URL, graph_id)
-    req = conn.get(url)
+    req = resolve_if_future(conn.get(url))
     if req.status_code == 200:
         return graph_id
     else:
         raise NotFound("No IPE graph found matching id: {}".format(graph_id))
 
+
 def register_ipe_graph(conn, ipe_graph):
     url = "{}/graph".format(VIRTUAL_IPE_URL)
-    res = conn.post(url, json.dumps(ipe_graph, sort_keys=True), headers={'Content-Type': 'application/json'})
+    res = resolve_if_future(conn.post(url, json.dumps(ipe_graph, sort_keys=True), headers={'Content-Type': 'application/json'}))
     return res.content
 
+
 def fetch_metadata(conn, url):
-    res = conn.get(url)
+    res = resolve_if_future(conn.get(url))
     res_json = res.json()
     if res.status_code != 200 or ('error' in res_json or 'message' in res_json):
         raise BadRequest("Problem fetching image metadata: {}".format(res.content))
@@ -27,11 +36,21 @@ def fetch_metadata(conn, url):
         return res_json
 
 def get_ipe_metadata(conn, ipe_id, node='toa_reflectance'):
-    meta = {}
-    meta['image'] = fetch_metadata(conn, VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node))
-    meta['rpcs'] = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/rpcs.json".format(ipe_id, node)).json()
+    image_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node))
+    georef_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node))
+    rpcs_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/rpcs.json".format(ipe_id, node))
+
+    image_response = resolve_if_future(image_response)
+    georef_response = resolve_if_future(georef_response)
+    rpcs_response = resolve_if_future(rpcs_response)
+
+    meta = {"image": image_response.json(), "rpcs": rpcs_response.json() }
+    if image_response.status_code != 200 or ('error' in meta["image"] or 'message' in meta["image"]):
+        raise BadRequest("Problem fetching image metadata: {}".format(res.content))
+
     try:
-        meta['georef'] = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node)).json()
+        meta["georef"] = georef_response.json()
     except:
-        meta['georef'] = None
+        meta["georef"] = None
+
     return meta
