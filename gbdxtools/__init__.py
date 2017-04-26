7ea9e1008789fb514aca89233f01da6975453771
Pramukta Kumar <pramukta.kumar@gmail.com>
2017-04-26 13:25:42 -0600

    refactoring the FuturesSession so that other pieces of gbdxtools  can more easily use it

--- gbdxtools/__init__.py
+++ gbdxtools/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+from requests_futures.sessions import FuturesSession
+_session = FuturesSession(max_workers=64)
 from gbdxtools.images.idaho_image import IdahoImage
 from gbdxtools.images.catalog_image import CatalogImage
 from gbdxtools.images.landsat_image import LandsatImage
