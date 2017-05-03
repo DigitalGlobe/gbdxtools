9202d0dd9ac4b83f26c367abaf3cfe6cc54f89dc
Jeffrey Erikson <jeffrey.erikson@digitalglobe.com>
2017-05-03 13:31:54 -0400

    Clean up vector aggregation example

--- examples/vectors_aggregation.py
+++ examples/vectors_aggregation.py
@@ -2,7 +2,6 @@ import gbdxtools
 from gbdxtools.vectors import TermsAggDef, GeohashAggDef
 
 gbdx = gbdxtools.Interface()
-gbdx.vectors.aggregations_url = 'https://devvector.geobigdata.io/insight-vector/api/aggregation'
 
 # Let's find all the Worldview 3 vector footprints in colorado
 colorado_aoi = "POLYGON((-108.89 40.87,-102.19 40.87,-102.19 37.03,-108.89 37.03,-108.89 40.87))"
@@ -18,8 +17,7 @@ result = gbdx.vectors.aggregate_query(colorado_aoi, agg, query, count=5)
 for entry in result[0]['terms']:  # the 'terms' field contains our buckets
     geohash_str = entry['term']  # the 'term' entry contains our geohash
     print geohash_str
-    child_aggs = entry[
-        'aggregations']  # the 'aggregations' field contains the child aggregations for the 'item_type' values
+    child_aggs = entry[ 'aggregations']  # the 'aggregations' field contains the child aggregations for the 'item_type' values
 
     # since the child aggregations have the same structure, we can walk it the same way.
     # let's create a dict of item_types and their counts
