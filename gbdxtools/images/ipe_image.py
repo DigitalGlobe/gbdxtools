8a3fa9ba41124d49d3494689caf07e6a26826aa3
Chris Helm <christopher.helm@gmail.com>
2017-05-19 10:02:14 -0600

    reverting to pycurl

--- gbdxtools/images/ipe_image.py
+++ gbdxtools/images/ipe_image.py
@@ -66,17 +66,37 @@ from gbdxtools.ipe.interface import Ipe
 from gbdxtools.auth import Auth
 ipe = Ipe()
 
-def load_url(url, token, bands=8):
+def load_url(url, size, token, bands=8):
     """ Loads a geotiff url inside a thread and returns as an ndarray """
-    try:
-        src = gdal.Open('/vsicurl/{}?token={}'.format(url, token))
-        arr = src.ReadAsArray()
-        if len(arr.shape) != 3:
-            arr = np.reshape(arr, (1,) + arr.shape)
-    except Exception as e:
-        print(e, url, '/vsicurl/{}?token={}'.format(url, token))
-        arr = np.zeros([bands,256,256], dtype=np.float32)
+    thread_id = threading.current_thread().ident
+    _curl = _curl_pool[thread_id]
+    buf = BytesIO()
+    _curl.setopt(_curl.URL, url)
+    _curl.setopt(_curl.WRITEDATA, buf)
+    _curl.setopt(pycurl.NOSIGNAL, 1)
+    _curl.setopt(pycurl.HTTPHEADER, ['Authorization: Bearer {}'.format(token)])
+    _curl.perform()
+    with MemoryFile(buf.getvalue()) as memfile:
+      try:
+          with memfile.open(driver="GTiff") as dataset:
+              arr = dataset.read()
+      except (TypeError, rasterio.RasterioIOError) as e:
+          arr = np.zeros([bands, size, size], dtype=np.float32)
+          _curl.close()
+          del _curl_pool[thread_id]
     return arr
+
+#@def load_url(url, token, bands=8):
+#@    """ Loads a geotiff url inside a thread and returns as an ndarray """
+#@    try:
+#@        src = gdal.Open('/vsicurl/{}?token={}'.format(url, token))
+#@        arr = src.ReadAsArray()
+#@        if len(arr.shape) != 3:
+#@            arr = np.reshape(arr, (1,) + arr.shape)
+#@    except Exception as e:
+#@        print(e, url, '/vsicurl/{}?token={}'.format(url, token))
+#@        arr = np.zeros([bands,256,256], dtype=np.float32)
+#@    return arr
 
 class DaskImage(da.Array):
     def __init__(self, **kwargs):
