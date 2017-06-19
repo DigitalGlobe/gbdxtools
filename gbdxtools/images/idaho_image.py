00ac558d389319cb90de65ec9a3ff7b7da01e755
Pramukta Kumar <pramukta.kumar@gmail.com>
2017-06-19 15:10:30 -0600

    replicating the original api (mostly)

--- gbdxtools/images/idaho_image.py
+++ gbdxtools/images/idaho_image.py
@@ -10,13 +10,48 @@ class IdahoImage(IpeImage):
       Dask based access to IDAHO images via IPE.
     """
     def __new__(cls, idaho_id, **kwargs):
+        options = {
+            "proj": kwargs.get("proj", "EPSG:4376"),
+            "product": kwargs.get("product", "toa_reflectance")
+        }
+
+        standard_products = cls._build_standard_products(idaho_id, options["proj"])
+        print(standard_products.keys())
+        try:
+            self = super(IdahoImage, cls).__new__(cls, standard_products[options["product"]])
+        except KeyError as e:
+            print(e)
+            print("Specified product not implemented: {}".format(options["product"]))
+            raise
+        self.idaho_id = idaho_id
+        self._products = standard_products
+        return self
+
+    def get_product(self, product):
+        return self.__class__(self.idaho_id, proj=self.proj, product=product)
+
+    @staticmethod
+    def _build_standard_products(idaho_id, proj):
+        dn_op = ipe.IdahoRead(bucketName="idaho-images", imageId=idaho_id, objectStore="S3")
+        ortho_op = ipe.Orthorectify(dn_op) #, **ortho_params(proj))
+
+        # TODO: Switch to direct metadata access (ie remove this block)
         idaho_md = requests.get('http://idaho.timbr.io/{}.json'.format(idaho_id)).json()
         meta = idaho_md["properties"]
         gains_offsets = calc_toa_gain_offset(meta)
         radiance_scales, reflectance_scales, radiance_offsets = zip(*gains_offsets)
-        ortho = ipe.Orthorectify(ipe.IdahoRead(bucketName="idaho-images", imageId=idaho_id, objectStore="S3"))
-        radiance = ipe.AddConst(ipe.MultiplyConst(ipe.Format(ortho, dataType="4"), constants=radiance_scales), constants=radiance_offsets)
-        toa_reflectance = ipe.MultiplyConst(radiance, constants=reflectance_scales)
-        self = super(IdahoImage, cls).__new__(cls, toa_reflectance)
-        self.idaho_id = idaho_id
-        return self
+        # ---
+
+        toa_reflectance_op = ipe.MultiplyConst(
+            ipe.AddConst(
+                ipe.MultiplyConst(
+                    ipe.Format(ortho_op, dataType="4"),
+                    constants=radiance_scales),
+                constants=radiance_offsets),
+            constants=reflectance_scales)
+
+        return {
+            "dn": dn_op,
+            "ortho": ortho_op,
+            "toa_reflectance": toa_reflectance_op
+        }
