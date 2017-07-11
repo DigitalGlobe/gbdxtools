e8fc3ff359f919cfb057fed4df9873bee99d8d2d
Chris Helm <christopher.helm@gmail.com>
2017-07-11 13:37:35 -0600

    testing resolve if future and travis tests

--- gbdxtools/ipe/graph.py
+++ gbdxtools/ipe/graph.py
@@ -23,7 +23,8 @@ def get_ipe_graph(conn, graph_id):
 
 def register_ipe_graph(conn, ipe_graph):
     url = "{}/graph".format(VIRTUAL_IPE_URL)
-    res = resolve_if_future(conn.post(url, json.dumps(ipe_graph, sort_keys=True), headers={'Content-Type': 'application/json'}))
+    res = conn.post(url, json.dumps(ipe_graph, sort_keys=True), headers={'Content-Type': 'application/json'}).result()
+    #res = resolve_if_future(conn.post(url, json.dumps(ipe_graph, sort_keys=True), headers={'Content-Type': 'application/json'}))
     return res.content
 
 
