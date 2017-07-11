ee707826058b7af6e89b8dea421801490c2a9a5a
Chris Helm <christopher.helm@gmail.com>
2017-07-11 11:16:25 -0600

    testing unified metadata

--- gbdxtools/ipe/graph.py
+++ gbdxtools/ipe/graph.py
@@ -28,21 +28,31 @@ def register_ipe_graph(conn, ipe_graph):
 
 
 def get_ipe_metadata(conn, ipe_id, node='toa_reflectance'):
-    image_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node)).result()
-    georef_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node)).result()
-    rpcs_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/rpcs.json".format(ipe_id, node)).result()
+    #image_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/image.json".format(ipe_id, node)).result()
+    #georef_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/georeferencing.json".format(ipe_id, node)).result()
+    #rpcs_response = conn.get(VIRTUAL_IPE_URL + "/metadata/idaho-virtual/{}/{}/rpcs.json".format(ipe_id, node)).result()
 
     #image_response = resolve_if_future(image_response)
     #georef_response = resolve_if_future(georef_response)
     #rpcs_response = resolve_if_future(rpcs_response)
+    
+    #meta = {"image": image_response.json(), "rpcs": rpcs_response.json() }
+    #try:
+    #    meta["georef"] = georef_response.json()
+    #except:
+    #    meta["georef"] = None
+    
 
-    meta = {"image": image_response.json(), "rpcs": rpcs_response.json() }
-    if image_response.status_code != 200 or ('error' in meta["image"] or 'message' in meta["image"]):
-        raise BadRequest("Problem fetching image metadata: {}".format(image_response.content))
+    md_response = conn.get(VIRTUAL_IPE_URL + "/metadata/{}/{}/metadata.json".format(ipe_id, node)).result()
+    md_json = md_response.json()
+    print(md_json.keys())
 
-    try:
-        meta["georef"] = georef_response.json()
-    except:
-        meta["georef"] = None
+    if md_response.status_code != 200 or ('error' in md_json or 'message' in md_json):
+        raise BadRequest("Problem fetching image metadata: {}".format(md_response.content))
 
+    meta = {
+        "image": md_json["imageMetadata"],
+        "georef": md_json.get("imageGeoreferencing", None),
+        "rpcs": md_json.get("rpcSensorModel", None)
+    }
     return meta
