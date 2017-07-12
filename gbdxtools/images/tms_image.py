a2505a4d8d05e8a55b667fa61d15bccf7053727d
Pramukta Kumar <pramukta.kumar@gmail.com>
2017-07-12 16:18:22 -0600

    typo

--- gbdxtools/images/tms_image.py
+++ gbdxtools/images/tms_image.py
@@ -160,7 +160,7 @@ class TmsImage(DaskImage, GeoImage):
             return image
         else:
             image = super(TmsImage, self).__getitem__(geometry)
-            if all([isisntance(e, slice), e in geometry]) and len(geometry) == len(self.shape):
+            if all([isinstance(e, slice), e in geometry]) and len(geometry) == len(self.shape):
                 # xmin, ymin, xmax, ymax
                 g = ops.transform(self.__geo_transform__.fwd,
                                   box(geometry[2].start, geometry[1].start, geometry[2].stop, geometry[1].stop))
