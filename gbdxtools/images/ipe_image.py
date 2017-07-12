f431fe11b20a35d99baae634c55bf37b313eba12
Chris Helm <christopher.helm@gmail.com>
2017-07-12 14:09:01 -0600

    reworking plot to support image class transforms for specific data access

--- gbdxtools/images/ipe_image.py
+++ gbdxtools/images/ipe_image.py
@@ -48,8 +48,32 @@ class IpeImage(DaskImage, GeoImage):
         return math.ceil((float(self.shape[-1]) / size)) * math.ceil(float(self.shape[1]) / size)
 
     @property
-    def rgb(self):
-        return [4,2,1]
+    def _rgb_bands(self):
+        return [4,2,1]    
+
+    @property
+    def _ndvi_bands(self):
+        return [7,4]
+
+    def rgb(self, **kwargs):
+        data = self[kwargs.get("bands", self._rgb_bands),...].read()
+        data = np.rollaxis(data.astype(np.float32), 0, 3)
+        lims = np.percentile(data, kwargs.get("stretch", [2,98]), axis=(0,1))
+        for x in xrange(len(data[0,0,:])):
+            top = lims[:,x][1]
+            bottom = lims[:,x][0]
+            data[:,:,x] = (data[:,:,x]-bottom)/float(top-bottom)
+        return np.clip(data,0,1)
+
+    def ndvi(self, **kwargs):
+        data = self[self._ndvi_bands,...].read().astype(np.float32)
+        return (data[0,:,:] - data[1,:,:]) / (data[0,:,:] + data[1,:,:])
+
+    def plot(self, spec="rgb", **kwargs):
+        if self.shape[0] == 1:
+            super(IpeImage, self).plot(w=w, h=h, cmap="Greys_r", tfm=lambda x: x[0,:,:])
+        else:
+            super(IpeImage, self).plot(tfm=getattr(self, spec), **kwargs)
 
     def __getitem__(self, geometry):
         if isinstance(geometry, BaseGeometry) or getattr(geometry, "__geo_interface__", None) is not None:
--- gbdxtools/images/landsat_image.py
+++ gbdxtools/images/landsat_image.py
@@ -27,25 +27,13 @@ class LandsatImage(IpeImage):
         self._products = standard_products
         return self.aoi(**kwargs)
 
-    def rgb(self, **kwargs):
-        data = self[[3,2,1],...].read()
-        data = np.rollaxis(data.astype(np.float32), 0, 3)
-        lims = np.percentile(data, kwargs.get("stretch", [2,98]), axis=(0,1))
-        for x in xrange(len(data[0,0,:])):
-            top = lims[:,x][1]
-            bottom = lims[:,x][0]
-            data[:,:,x] = (data[:,:,x]-bottom)/float(top-bottom)
-        return np.clip(data,0,1)
+    @property
+    def _rgb_bands(self):
+        return [3,2,1]
 
-    def ndvi(self, **kwargs):
-        data = self[[4,3],...].read().astype(np.float32)
-        return (data[0,:,:] - data[1,:,:]) / (data[0,:,:] + data[1,:,:])
-
-    def plot(self, spec="rgb", **kwargs): 
-        if self.shape[0] == 1:
-            super(LandsatImage, self).plot(w=w, h=h, cmap="Greys_r", tfm=lambda x: x[0,:,:])
-        else:
-            super(LandsatImage, self).plot(tfm=getattr(self, spec), **kwargs)
+    @property
+    def _ndvi_bands(self):
+        return [4,3]
 
     def get_product(self, product):
         return self.__class__(self._id, proj=self.proj, product=product)
