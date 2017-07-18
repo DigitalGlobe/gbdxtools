15082a5562e865d029b8ec0999da8b7972373a26
Pramukta Kumar <pramukta.kumar@gmail.com>
2017-07-18 10:04:13 -0600

    promoting TmsImage and DemImage to top level namespace

--- gbdxtools/__init__.py
+++ gbdxtools/__init__.py
@@ -3,4 +3,6 @@ from gbdxtools.images.idaho_image import IdahoImage
 from gbdxtools.images.catalog_image import CatalogImage
 from gbdxtools.images.landsat_image import LandsatImage
 from gbdxtools.images.tms_image import TmsImage
+from gbdxtools.images.dem_image import DemImage
+from gbdxtools.images.tms_image import TmsImage
 from .interface import Interface
