3d7b06fbbf7193751938ccc5fcef0f22c8c0c7cc
Pramukta Kumar <pramukta.kumar@gmail.com>
2017-08-07 13:55:14 -0500

    progress on ortho

--- gbdxtools/images/util.py
+++ gbdxtools/images/util.py
@@ -3,15 +3,16 @@ import numpy as np
 import skimage.transform as tf
 
 def orthorectify(img, z=0):
+    # TODO: potientially reproject
     xmin, ymin, xmax, ymax = shape(img).bounds
     data = img.read()
-    x, xsamp = np.linspace(xmin, xmax, num=data.shape[-1], retstep=True)
-    y, ysamp = np.linspace(ymax, ymin, num=data.shape[1], retstep=True)
+    gsd = max((xmax-xmin)/data.shape[2], (ymax-ymin)/data.shape[1])
+    print("gsd:", gsd)
+    x = np.linspace(xmin, xmax, num=int((xmax-xmin)/gsd))
+    y = np.linspace(ymax, ymin, num=int((ymax-ymin)/gsd))
     xv, yv = np.meshgrid(x, y, indexing='xy')
     if isinstance(z, np.ndarray):
+        # TODO: potentially reproject
         z = tf.resize(z[0,:,:], data.shape[1:])
     transpix = img.__geo_transform__.rev(xv, yv, z=z, _type=np.float32)[::-1]
-    transpix[0,:,:] -= transpix[0,0,0]
-    transpix[1,:,:] -= transpix[1,0,0]
     return np.rollaxis(np.dstack([tf.warp(data[b,:,:].squeeze(), transpix, preserve_range=True) for b in xrange(data.shape[0])]), 2, 0)
-    
