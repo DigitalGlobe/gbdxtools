4d28da74627b933d7374719dcce8d6beb737a564
Chris Helm <christopher.helm@gmail.com>
2017-08-08 15:19:16 -0600

    rm vrt and its utils

--- gbdxtools/ipe/util.py
+++ gbdxtools/ipe/util.py
@@ -44,17 +44,6 @@ def ortho_params(proj):
     return params
 
 
-# StackOverflow: http://stackoverflow.com/questions/600268/mkdir-p-functionality-in-python
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
-
-
 def calc_toa_gain_offset_new(meta):
     """
     Compute (gain, offset) tuples for each band of the specified image metadata
