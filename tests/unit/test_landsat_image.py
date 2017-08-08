042d780e38836b4311df67db72b868e5024ba704
Chris Helm <christopher.helm@gmail.com>
2017-08-08 14:20:45 -0600

    adding test for proj in landsat

--- tests/unit/test_landsat_image.py
+++ tests/unit/test_landsat_image.py
@@ -55,3 +55,11 @@ class IpeImageTest(unittest.TestCase):
         self.assertTrue(isinstance(img, LandsatImage))
         assert img.shape == (8, 566, 685)
         assert img.proj == 'EPSG:32612'
+
+    @my_vcr.use_cassette('tests/unit/cassettes/test_landsat_image_proj.yaml', filter_headers=['authorization'])
+    def test_landsat_image_proj(self):
+        _id = 'LC80370302014268LGN00'
+        img = self.gbdx.landsat_image(_id, bbox=[-109.84, 43.19, -109.59, 43.34], proj="EPSG:4326")
+        self.assertTrue(isinstance(img, LandsatImage))
+        assert img.shape == (8, 474, 790)
+        assert img.proj == 'EPSG:4326'
