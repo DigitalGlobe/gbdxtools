5d037f5ef089b98cb7adde7cd525ed18d69f359f
Chris Helm <christopher.helm@gmail.com>
2017-08-10 21:12:02 -0600

    no more certifi

--- gbdxtools/ipe/interface.py
+++ gbdxtools/ipe/interface.py
@@ -52,7 +52,7 @@ def load_url(url, token, shape=(8, 256, 256)):
     """ Loads a geotiff url inside a thread and returns as an ndarray """
     thread_id = threading.current_thread().ident
     _curl = _curl_pool[thread_id]
-    _curl.setopt(pycurl.CAINFO, certifi.where())
+    #_curl.setopt(pycurl.CAINFO, certifi.where())
     _curl.setopt(_curl.URL, url)
     _curl.setopt(pycurl.NOSIGNAL, 1)
     _curl.setopt(pycurl.HTTPHEADER, ['Authorization: Bearer {}'.format(token)])
