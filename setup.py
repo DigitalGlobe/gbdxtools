df34200113643f497cca4e21ccf736e00688d858
Nate Ricklin <nate@rickl.in>
2017-08-18 15:40:19 -0600

    Bump version: 0.12.0 → 0.13.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("requirements.txt") as f:
 # long_description=readme,
 
 setup(name='gbdxtools',
-      version='0.12.0',
+      version='0.13.0',
       description='Additional s3 functionality.',
       classifiers=[],
       keywords='',
