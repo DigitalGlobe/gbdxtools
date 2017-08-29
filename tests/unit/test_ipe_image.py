a4a8a20c8d21ec9362120c6e22e631ac1e0b5481
Chris Helm <christopher.helm@gmail.com>
2017-08-29 08:55:28 -0600

    adding ortho test

--- tests/unit/test_ipe_image.py
+++ tests/unit/test_ipe_image.py
@@ -121,7 +121,7 @@ class IpeImageTest(unittest.TestCase):
         img = self.gbdx.idaho_image(idahoid, product="1b", bbox=[-110.85299491882326,32.167148499672855,-110.84870338439943,32.170236308395644])
         #assert img.ipe_id == '414c35956f15a521d35012ec2cb60a8ee2fa7492e7b5f0fa0c6999a580543749'
         assert isinstance(img.ipe_metadata, dict)
-        assert img._ndvi_bands == [7, 4]
+        assert img._ndvi_bands == [6, 4]
         assert img.shape == (8, 176, 203)
         assert isinstance(img, da.Array)
 
@@ -143,15 +143,15 @@ class IpeImageTest(unittest.TestCase):
     #    rgb = aoi.rgb()
     #    assert isinstance(rgb, np.ndarray)
     
-    #@my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_ortho.yaml', filter_headers=['authorization'])
-    #def test_ipe_image_ortho(self):
-    #    idahoid = '179269b9-fdb3-49d8-bb62-d15de54ad15d'
-    #    img = self.gbdx.idaho_image(idahoid, product='1b')
-    #    aoi = img.aoi(bbox=[-110.85299491882326,32.167148499672855,-110.84870338439943,32.170236308395644])
-    #    assert aoi.shape == (8, 176, 203)
-    #    aoi.ortho = read_mock
-    #    ortho = aoi.warp()
-    #    assert isinstance(ortho, DaskImage)
+    @my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_ortho.yaml', filter_headers=['authorization'])
+    def test_ipe_image_ortho(self):
+        idahoid = '179269b9-fdb3-49d8-bb62-d15de54ad15d'
+        img = self.gbdx.idaho_image(idahoid, product='1b')
+        aoi = img.aoi(bbox=[-110.85299491882326,32.167148499672855,-110.84870338439943,32.170236308395644])
+        assert aoi.shape == (8, 176, 203)
+        aoi.ortho = read_mock
+        ortho = aoi.warp()
+        assert isinstance(ortho, DaskImage)
 
     #@my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_geotiff.yaml', filter_headers=['authorization'])
     #def test_ipe_image_geotiff(self):
