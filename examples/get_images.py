4f9ab2d5a961a81248b4f24de70e2e4dbba52bb3
adeaner <ashley.deaner@digitalglobe.com>
2017-08-29 17:03:39 -0600

    fix formatting

--- examples/get_images.py
+++ examples/get_images.py
@@ -1,6 +1,5 @@
 # Order a list of cat ids, launch AOP workflow for each one and download locally.
 
-import json
 import time
 
 from gbdxtools import Interface
@@ -13,26 +12,26 @@ catalog_ids = ['1030010034AFCE00',
                '1050410011360600',
                '1050410011360700']
 
-print 'Order imagery from GBDX'
+print('Order imagery from GBDX')
 gbdx = Interface()
-order_id = gbdx.ordering.order(catalog_ids) 
+order_id = gbdx.ordering.order(catalog_ids)
 
 # check order status
 while True:
-    states, locations = zip(*[(order['state'],order['location']) 
-                            for order in gbdx.ordering.status(order_id)])
+    states, locations = zip(*[(order['state'], order['location'])
+                              for order in gbdx.ordering.status(order_id)])
     if any(state != 'delivered' for state in states):
         time.sleep(300)
     else:
-        break    
+        break
 
-print 'Elapsed time: {} min'.format(round((time.time() - start_time)/60))
+print('Elapsed time: {} min'.format(round((time.time() - start_time) / 60)))
 
 # where in bucket/prefix to store my AOP'ed imagery
 s3_location = 'kostas/yunnanearthquake2014'
 
 # launch AOP workflows
-print 'Launch AOP workflows'
+print('Launch AOP workflows')
 workflow_ids = [gbdx.workflow.launch_aop_to_s3(location,
                                                s3_location,
                                                enable_acomp='true',
@@ -41,14 +40,14 @@ workflow_ids = [gbdx.workflow.launch_aop_to_s3(location,
 # check workflow status
 pending_workflow_ids = workflow_ids
 while len(pending_workflow_ids) > 0:
-    print 'Pending workflows', pending_workflow_ids
+    print('Pending workflows', pending_workflow_ids)
     for workflow_id in pending_workflow_ids:
         result = gbdx.workflow.status(workflow_id)
         if result['state'] == 'complete':
             pending_workflow_ids.remove(workflow_id)
     time.sleep(300)
 
-print 'Elapsed time: {} min'.format(round((time.time() - start_time)/60))
+print('Elapsed time: {} min'.format(round((time.time() - start_time) / 60)))
 
 # download
 gbdx.s3.download(s3_location)
