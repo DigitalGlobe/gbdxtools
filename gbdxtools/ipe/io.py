93b0b9953358f2e7ab21f8c7454189115664eae7
Chris Helm <christopher.helm@gmail.com>
2017-10-03 08:43:53 -0600

    using windowed writes for geotiffs to handle large images

--- gbdxtools/ipe/io.py
+++ gbdxtools/ipe/io.py
@@ -1,13 +1,16 @@
 import rasterio
 import numpy as np
 
+def chunk_to_slice(n, size):
+    start = n * 256
+    end = start + size
+    return start, end
 
 def to_geotiff(arr, path='./output.tif', proj=None, dtype=None, bands=None, **kwargs):
     if dtype is not None:
         dtype = np.dtype(dtype)
-    data = arr.read(bands=bands)
-    c,h,w = data.shape
-    try: 
+    c,h,w = arr.shape
+    try:
         tfm = kwargs['transform'] if 'transform' in kwargs else arr.affine
     except:
         tfm = None
@@ -15,15 +18,22 @@ def to_geotiff(arr, path='./output.tif', proj=None, dtype=None, bands=None, **kw
         'width': w,
         'height': h,
         'count': c,
-        'dtype': data.dtype.name if dtype is None else dtype.name,
+        'dtype': arr.dtype.name if dtype is None else dtype.name,
         'driver': 'GTiff',
         'transform': tfm
     }
     if proj is not None:
         meta["crs"] = {'init': proj}
     with rasterio.open(path, "w", **meta) as dst:
-        if dtype is not None:
-            data = (data - data.min()) * (float(np.iinfo(dtype).max) * data.max())
-            data = data.astype(dtype)
-        dst.write(data)
+        bs, ys, xs = arr.chunks
+        for i, y in enumerate(ys):
+            for j, x in enumerate(xs):
+                ystart,yend = chunk_to_slice(i, y)
+                xstart,xend = chunk_to_slice(j, x)
+                region = arr[:, ystart:yend, xstart:xend]
+                data = region.read(bands=bands, quiet=1)
+                if dtype is not None:
+                    data = (data - data.min()) * (float(np.iinfo(dtype).max) * data.max())
+                    data = data.astype(dtype)
+                dst.write(data, window=((ystart, yend), (xstart, xend)))
     return path
