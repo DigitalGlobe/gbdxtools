296e1599e8de773c7731b141083a91eac668d2c0
Chris Helm <christopher.helm@gmail.com>
2017-10-05 08:52:08 -0600

    new style of geotiff creation via dask store :snake:

--- gbdxtools/ipe/io.py
+++ gbdxtools/ipe/io.py
@@ -1,39 +1,47 @@
 import rasterio
 import numpy as np
+from dask.array import store
 
-def chunk_to_slice(n, size):
-    start = n * 256
-    end = start + size
-    return start, end
+class rio_writer(object):
+    def __init__(self, dst):
+        self.dst = dst
+        
+    def __setitem__(self, location, chunk):
+        window = ((location[1].start, location[1].stop), 
+                  (location[2].start, location[2].stop))
+        self.dst.write(chunk, window=window)
 
-def to_geotiff(arr, path='./output.tif', proj=None, dtype=None, bands=None, **kwargs):
-    if dtype is not None:
-        dtype = np.dtype(dtype)
-    c,h,w = arr.shape
+def to_geotiff(arr, path='./output.tif', proj=None, bands=None, **kwargs):
+    if bands is not None:
+        arr = arr[bands,...]
+
+    try:
+        img_md = arr.ipe.metadata["image"]
+        x_size = img_md["tileXSize"]
+        y_size = img_md["tileYSize"]
+    except (AttributeError, KeyError):
+        x_size = kwargs.get("chunk_size", 256)
+        y_size = kwargs.get("chunk_size", 256)
+
     try:
         tfm = kwargs['transform'] if 'transform' in kwargs else arr.affine
     except:
         tfm = None
+
     meta = {
-        'width': w,
-        'height': h,
-        'count': c,
-        'dtype': arr.dtype.name if dtype is None else dtype.name,
+        'width': arr.shape[2],
+        'height': arr.shape[1],
+        'count': arr.shape[0],
+        'dtype': arr.dtype.name,
         'driver': 'GTiff',
         'transform': tfm
     }
     if proj is not None:
         meta["crs"] = {'init': proj}
+    meta.update(blockxsize=x_size, blockysize=y_size, tiled='yes')
+
     with rasterio.open(path, "w", **meta) as dst:
-        bs, ys, xs = arr.chunks
-        for i, y in enumerate(ys):
-            for j, x in enumerate(xs):
-                ystart,yend = chunk_to_slice(i, y)
-                xstart,xend = chunk_to_slice(j, x)
-                region = arr[:, ystart:yend, xstart:xend]
-                data = region.read(bands=bands, quiet=True)
-                if dtype is not None:
-                    data = (data - data.min()) * (float(np.iinfo(dtype).max) * data.max())
-                    data = data.astype(dtype)
-                dst.write(data, window=((ystart, yend), (xstart, xend)))
+        writer = rio_writer(dst)
+        store(arr, writer)
+    
     return path
