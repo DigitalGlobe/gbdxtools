53bf437771a34efe935a4fcebc821ed37c0149d7
Chris Helm <christopher.helm@gmail.com>
2018-01-09 15:50:30 -0700

    adding bucket name to idaho image inits

--- gbdxtools/images/worldview.py
+++ gbdxtools/images/worldview.py
@@ -66,7 +66,10 @@ class WVImage(IpeImage):
     def parts(self):
         if self._parts is None:
             self._parts = [IdahoImage(rec['properties']['attributes']['idahoImageId'],
-                                      product=self.options["product"], proj=self.options["proj"], gsd=self.options["gsd"])
+                                      product=self.options["product"], 
+                                      proj=self.options["proj"], 
+                                      bucket=rec['properties']['attributes']['bucketName']
+                                      gsd=self.options["gsd"])
                            for rec in self._find_parts(self.cat_id, self.options["band_type"])]
         return self._parts
 
