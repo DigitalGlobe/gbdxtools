c955ed584eda4a664a1027ed560f4b901891b679
Chris Helm <christopher.helm@gmail.com>
2018-02-02 12:05:09 -0700

    updating s3 to use boto3; also adding s3.upload

--- gbdxtools/s3.py
+++ gbdxtools/s3.py
@@ -6,7 +6,7 @@ Contact: kostas.stamatiou@digitalglobe.com
 import os
 from builtins import object
 
-from boto import s3 as botos3
+import boto3
 from gbdxtools.auth import Auth
 
 class S3(object):
@@ -28,6 +28,15 @@ class S3(object):
         self.logger = interface.logger
 
         self._info = None
+        self._client = None
+
+    @property
+    def client(self):
+        if self._client is None:
+            self._client = boto3.client('s3', aws_access_key_id=self.info['S3_access_key'], 
+                                              aws_secret_access_key=self.info['S3_secret_key'],
+                                              aws_session_token=self.info['S3_session_token'])
+        return self._client
 
     @property
     def info(self):
@@ -70,33 +79,26 @@ class S3(object):
         self.logger.debug('Getting S3 info')
         bucket = self.info['bucket']
         prefix = self.info['prefix']
-        access_key = self.info['S3_access_key']
-        secret_key = self.info['S3_secret_key']
-        session_token = self.info['S3_session_token']
 
         self.logger.debug('Connecting to S3')
-        s3conn = botos3.connect_to_region('us-east-1', aws_access_key_id=access_key,
-                                          aws_secret_access_key=secret_key,
-                                          security_token=session_token)
-
-        b = s3conn.get_bucket(bucket, validate=False,
-                              headers={'x-amz-security-token': session_token})
+        s3conn = self.client
 
         # remove head and/or trail backslash from location
         location = location.strip('/')
 
-        whats_in_here = b.list(prefix + '/' + location)
-
         self.logger.debug('Downloading contents')
-        for key in whats_in_here:
+        for s3key in s3conn.list_objects(Bucket=bucket, Prefix=os.path.join(prefix, location))['Contents']:
+            key = s3key['Key']
+    
             # skip directory keys
-            if not key.name or key.name.endswith('/'):
+            if not key or key.endswith('/'):
                 continue
 
             # get path to each file
-            filepath = key.name.replace(prefix + '/' + location, '', 1).lstrip('/')
-            filename = key.name.split('/')[-1]
-            self.logger.debug(filename)
+            filepath = key.replace(os.path.join(prefix, location), '', 1).lstrip('/')
+            filename = key.split('/')[-1]
+            
+            #self.logger.debug(filename)
             file_dir = filepath.split('/')[:-1]
             file_dir = '/'.join(file_dir)
             full_dir = os.path.join(local_dir, file_dir)
@@ -106,7 +108,7 @@ class S3(object):
                 os.makedirs(full_dir)
 
             # download file
-            key.get_contents_to_filename(full_dir + '/' + filename)
+            s3conn.download_file(bucket, key, os.path.join(full_dir, filename))
 
         self.logger.debug('Done!')
 
@@ -120,20 +122,11 @@ class S3(object):
                location (str): S3 location within prefix. Can be a directory or
                                a file (e.g., my_dir or my_dir/my_image.tif).
         '''
-
         bucket = self.info['bucket']
         prefix = self.info['prefix']
-        access_key = self.info['S3_access_key']
-        secret_key = self.info['S3_secret_key']
-        session_token = self.info['S3_session_token']
 
         self.logger.debug('Connecting to S3')
-        s3conn = botos3.connect_to_region('us-east-1', aws_access_key_id=access_key,
-                                      aws_secret_access_key=secret_key,
-                                      security_token=session_token)
-
-        b = s3conn.get_bucket(bucket, validate=False,
-                              headers={'x-amz-security-token': session_token})
+        s3conn = self.client 
 
         # remove head and/or trail backslash from location
         if location[0] == '/':
@@ -141,11 +134,29 @@ class S3(object):
         if location[-1] == '/':
             location = location[:-2]
 
-        whats_in_here = b.list(prefix + '/' + location)
-
         self.logger.debug('Deleting contents')
 
-        for key in whats_in_here:
-            b.delete_key(key)
+        for s3key in s3conn.list_objects(Bucket=bucket, Prefix=os.path.join(prefix, location))['Contents']:
+            s3conn.delete_object(Bucket=bucket, Key=s3key['Key'])
 
         self.logger.debug('Done!')
+
+    def upload(self, local_file, s3_path=''):
+        '''
+        Upload files to your DG S3 bucket/prefix.
+
+        Args:
+            local_file (str): a path to a local file to upload
+            s3_path: a location on s3 to upload the file to
+        '''
+        bucket = self.info['bucket']
+        prefix = self.info['prefix']
+
+        self.logger.debug('Connecting to S3')
+        s3conn = self.client 
+        self.logger.debug('Uploading file {}'.format(local_file))
+        s3conn.upload_file(local_file, bucket, os.path.join(prefix, s3_path))
+        self.logger.debug('Done!')
+
+
+        
