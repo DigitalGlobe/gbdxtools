b891049391a2e2d7af013e902a4da32dc2e490c2
brett55 <bjcull2@gmail.com>
2018-02-21 16:18:27 -0700

    trying delete false

--- tests/unit/test_401_retry.py
+++ tests/unit/test_401_retry.py
@@ -33,7 +33,7 @@ user_password = your_password
 """
 
 # create temp gbdx-config file
-temp = tempfile.NamedTemporaryFile(dir=os.getcwd(), suffix=".ini", mode="w+t")
+temp = tempfile.NamedTemporaryFile(dir=os.getcwd(), suffix=".ini", mode="w+t", delete=False)
 # write the data
 temp.write(data)
 temp.seek(0)
