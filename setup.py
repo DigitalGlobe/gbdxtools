9ba6da6d781e66383ed49f0482138f10c124745f
Chris Helm <christopher.helm@gmail.com>
2018-02-22 12:31:28 -0700

    Bump version: 0.14.4 → 0.14.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(req_path) as f:
 # long_description=readme,
 
 setup(name='gbdxtools',
-      version='0.14.4',
+      version='0.14.5',
       description='Additional s3 functionality.',
       classifiers=[],
       keywords='',
