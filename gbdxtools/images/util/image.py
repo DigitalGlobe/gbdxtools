d6db4798e8c3e08485882665ab658f95458bf85c
Jamie Polackwich <rjpolackwich@gmail.com>
2018-04-01 03:55:42 -0400

    moving reproject_params here used all over the place

--- gbdxtools/images/util/image.py
+++ gbdxtools/images/util/image.py
@@ -10,6 +10,14 @@ band_types = {
     'pan': 'PAN'
 }
 
+def reproject_params(proj):
+    _params = {}
+    if proj is not None:
+        _params["Source SRS Code"] = "EPSG:4326"
+        _params["Source pixel-to-world transform"] = None
+        _params["Dest SRS Code"] = proj
+        _params["Dest pixel-to-world transform"] = None
+    return _params
 
 def vendor_id(rec):
     _id = rec['properties']['attributes']['vendorDatasetIdentifier']
