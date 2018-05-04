cda0b44e059763903f26ac481fdbc76c4d4a231f
mpfister <mpfister@mpfister-MacBook-Pro.local>
2018-05-04 14:55:53 -0600

    Add pxbounds method to images; tests

--- gbdxtools/images/meta.py
+++ gbdxtools/images/meta.py
@@ -261,6 +261,43 @@ class GeoDaskImage(DaskImage, Container, PlotMixin):
         else:
             return self[g]
 
+    def pxbounds(self, geom):
+        """ Returns the bounds of a geometry object in pixel coordinates
+
+        args:
+            geom: Shapely geometry object or GeoJSON as Python dictionary or WKT string
+
+        Returns:
+            list of bounds in pixels [min x, min y, max x, max y] clipped to image bounds
+        """
+
+        try:
+            if isinstance(geom, dict):
+                if 'geometry' in geom:
+                    geom = shape(geom['geometry'])
+                else:
+                    geom = shape(geom)
+            elif isinstance(geom, BaseGeometry):
+                geom = shape(geom)
+            else:
+                geom = wkt.loads(geom)
+        except:
+            raise TypeError ("Invalid geometry object")
+
+        # if geometry doesn't overlap the image, return an error
+        if geom.disjoint(shape(self)):
+            raise ValueError("Geometry outside of image bounds")
+        
+        # clip to pixels within the image
+        (xmin, ymin, xmax, ymax) = ops.transform(self.__geo_transform__.rev, geom).bounds 
+        _nbands, ysize, xsize = self.shape
+        xmin = max(xmin, 0)
+        ymin = max(ymin, 0)
+        xmax = min(xmax, xsize)
+        ymax = min(ymax, ysize)
+
+        return (xmin, ymin, xmax, ymax)
+
     def geotiff(self, **kwargs):
         """ Creates a geotiff on the filesystem
 
