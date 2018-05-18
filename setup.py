4e630f4f6e678299d0cae11363b2f68d49a8fbe5
Chris Helm <christopher.helm@gmail.com>
2018-05-18 09:56:40 -0600

    Bump version: 0.15.0 → 0.15.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(req_path) as f:
 # long_description=readme,
 
 setup(name='gbdxtools',
-      version='0.15.0',
+      version='0.15.1',
       description='API wrapper and imagery access for the GBDX Platform',
       classifiers=[],
       keywords='',
