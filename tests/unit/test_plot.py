836a302029e29808178da1cd32accc7cb360d974
drwelby <marc.pfister@digitalglobe.com>
2018-06-21 14:20:18 -0600

    basic plot tests

---
+++ tests/unit/test_plot.py
@@ -0,0 +1,42 @@
+import unittest
+from numpy import zeros
+from gbdxtools.images.mixins.geo import PlotMixin
+
+
+class PlotMock(np.ndarray, PlotMixin):
+
+    @property
+    def _rgb_bands(self):
+        return [3,1,2]
+
+    @property
+    def _ndvi_bands(self):
+        return [6,3]
+
+    def _read(self, arr):
+        return arr
+
+
+class PlotTest(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.zero = np.zeros((8,8,8))
+        cls.plotmock = cls.zero.view(PlotMock) 
+
+    def test_plot_rgb(self):
+        cls.plotmock.plot()
+
+    def test_plot_ndvi(self):
+        cls.plotmock.plot(spec="ndvi")
+
+    def test_plot_1_band(self):
+        cls.plotmock.plot(bands=[1])
+
+    def test_plot_3_band(self):
+        cls.plotmock.plot(bands=[3,2,1])
+
+    def test_plot_4_band_fail(self):
+        with self.assertRaises(AssertionError):
+            cls.plotmock.plot(bands=[4,3,2,1])
