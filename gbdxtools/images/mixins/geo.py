cd3cd9ef925bb5cc14821aa4877cf5ee817966e4
martham93 <marthamorrissey@berkeley.edu>
2018-07-19 11:39:17 -0600

    adde ndvi doc string

--- gbdxtools/images/mixins/geo.py
+++ gbdxtools/images/mixins/geo.py
@@ -53,6 +53,11 @@ class PlotMixin(object):
         return np.clip(data, 0, 1)
 
     def ndvi(self, **kwargs):
+        """
+        Calculates Normalized Difference Vegetation Index using NIR and Red of an image.
+
+        Returns: numpy array with ndvi values
+        """
         data = self._read(self[self._ndvi_bands,...]).astype(np.float32)
         return (data[0,:,:] - data[1,:,:]) / (data[0,:,:] + data[1,:,:])
 
