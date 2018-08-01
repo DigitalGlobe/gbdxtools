d6683366dd7d29f46b308f08fa781a54d58d2f86
Jamie Polackwich <rjpolackwich@gmail.com>
2018-08-01 00:20:22 -0600

    Mirror the rda module by setting the module path explicitly; issue deprecation warning on module load

--- gbdxtools/ipe/__init__.py
+++ gbdxtools/ipe/__init__.py
@@ -1,1 +1,7 @@
 import sys
+import gbdxtools.rda
+from gbdxtools.rda.util import deprecation
+
+deprecation("The module 'gbdxtools.ipe' has been deprecated, functionality has been moved to gbdxtools.rda")
+
+sys.modules[__name__] = sys.modules['gbdxtools.rda']
