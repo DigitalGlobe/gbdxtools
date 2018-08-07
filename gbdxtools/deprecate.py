e522b6440fc60b6bfd1f7758c1464270a5c0e628
Chris Helm <christopher.helm@gmail.com>
2018-08-07 11:08:36 -0600

    using a custom deprecation class to avoid logging deprecation warnings from other libs

--- gbdxtools/deprecate.py
+++ gbdxtools/deprecate.py
@@ -1,28 +1,38 @@
 import sys, warnings
 import functools
 
+class GBDXDeprecation(DeprecationWarning):
+    pass
+
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.simplefilter("always", GBDXDeprecation)
+
+def deprecation(message):
+    warnings.warn(message, GBDXDeprecation)
+
 def deprecate_module_attr(mod, deprecated):
     """Return a wrapped object that warns about deprecated accesses"""
     deprecated = set(deprecated)
     class Wrapper(object):
         def __getattr__(self, attr):
             if attr in deprecated:
-                warnings.warn("Property {} is deprecated".format(attr), DeprecationWarning)
+                warnings.warn("Property {} is deprecated".format(attr), GBDXDeprecation)
 
             return getattr(mod, attr)
 
         def __setattr__(self, attr, value):
             if attr in deprecated:
-                warnings.warn("Property {} is deprecated".format(attr), DeprecationWarning)
+                warnings.warn("Property {} is deprecated".format(attr), GBDXDeprecation)
             return setattr(mod, attr, value)
     return Wrapper()
 
 def deprecate_class(klass, dep_name):
     @functools.wraps(klass)
     def wrappedklass(*args, **kwargs):
-        warnings.simplefilter("always", DeprecationWarning)
+        warnings.simplefilter("ignore", DeprecationWarning)
+        warnings.simplefilter("always", GBDXDeprecation)
         warnings.warn("Class {} has been deprecated, use {} instead".format(dep_name, klass.__name__),
-                      DeprecationWarning)
-        warnings.simplefilter("default", DeprecationWarning)
+                      GBDXDeprecation)
+        warnings.simplefilter("default", GBDXDeprecation)
         return klass(*args, **kwargs)
     return wrappedklass
