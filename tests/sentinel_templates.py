79d5798758870875b3750685a25602fd769c0c34
brett55 <bjcull2@gmail.com>
2019-04-10 13:16:02 -0600

    working sentinel 1 & 2 image with template api

---
+++ tests/sentinel_templates.py
@@ -0,0 +1,58 @@
+Sentinel2 = {
+    "defaultNodeId": "SentinelRead",
+    "edges": [{
+        "destination": "Reproject",
+        "id": "edge-1",
+        "index": 1,
+        "source": "Sentinel2Read"
+    }],
+    "nodes": [{
+        "id": "Reproject",
+        "operator": "Reproject",
+        "parameters": {
+            "Dest SRS Code": "${DestSRSCode}",
+            "Dest pixel-to-world transform": "null",
+            "Source SRS Code": "${SourceSRSCode}",
+            "Source pixel-to-world transform": "null"
+        }
+    },
+        {
+            "id": "Sentinel2Read",
+            "operator": "Sentinel2Read",
+            "parameters": {
+                "SentinelId": "${sentinelId}",
+                "sentinelProductSpec": "${sentinelProductSpec}"
+            }
+        }
+    ]
+}
+
+Sentinel1 = {
+    "defaultNodeId": "SentinelRead",
+    "edges": [{
+        "destination": "Reproject",
+        "id": "edge-1",
+        "index": 1,
+        "source": "Sentinel1Read"
+    }],
+    "nodes": [{
+        "id": "Reproject",
+        "operator": "Reproject",
+        "parameters": {
+            "Dest SRS Code": "${DestSRSCode}",
+            "Dest pixel-to-world transform": "null",
+            "Source SRS Code": "${SourceSRSCode}",
+            "Source pixel-to-world transform": "null"
+        }
+    },
+        {
+            "id": "Sentinel1Read",
+            "operator": "Sentinel1Read",
+            "parameters": {
+                "SentinelId": "${sentinelId}",
+                "sentinel1Polarization": "${sentinel1Polarization}"
+            }
+        }
+    ]
+}
+
