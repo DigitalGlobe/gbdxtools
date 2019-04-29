18a0f3e417bafae59b11c5699cb5eae336b2dd60
brett55 <bjcull2@gmail.com>
2019-04-29 15:58:15 -0600

    fixed conflicts

--- tests/unit/test_image_utils.py
+++ tests/unit/test_image_utils.py
@@ -3,20 +3,21 @@ Unit tests for the generic image class functionality
 '''
 
 from gbdxtools import CatalogImage
-from auth_mock import gbdx 
+from auth_mock import gbdx
 import vcr
-import tempfile
 import unittest
+
 
 def force(r1, r2):
     return True
+
 
 my_vcr = vcr.VCR()
 my_vcr.register_matcher('force', force)
 my_vcr.match_on = ['force']
 
-class ImageUtilTest(unittest.TestCase):
 
+class ImageUtilTest(unittest.TestCase):
     _temp_path = None
 
     @classmethod
@@ -32,9 +33,17 @@ class ImageUtilTest(unittest.TestCase):
 
     @my_vcr.use_cassette('tests/unit/cassettes/test_image_ordered.yaml', filter_headers=['authorization'])
     def test_image_ordered(self):
-        ordered = CatalogImage.is_ordered('1050410000B1BD00')
-        self.assertFalse(ordered)
         ordered = CatalogImage.is_ordered('104001002ABEA500')
+        self.assertFalse(ordered)
+        ordered = CatalogImage.is_ordered('1030010076B8F500')
         self.assertTrue(ordered)
 
-
+    @my_vcr.use_cassette('tests/unit/cassettes/test_image_ordered_fails_with_non_dg_cat_id.yaml', filter_headers=['authorization'])
+    def test_image_ordered_fails_with_non_dg_cat_id(self):
+        """
+        Test that a non DG cat Id raises exception when checking `is_ordered` method
+        :return:
+        """
+        with self.assertRaises(Exception):
+            # landsat is not a DG catalog Id
+            CatalogImage.is_ordered('LC80380302013160LGN00')
