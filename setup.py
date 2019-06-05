68d7ae176ba7ac824d09eb3889105585018e938f
drwelby <marc.pfister@digitalglobe.com>
2019-06-05 14:50:51 -0600

    Bump version: 0.16.7 → 0.16.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(req_path) as f:
 # long_description=readme,
 
 setup(name='gbdxtools',
-      version='0.16.7',
+      version='0.16.8',
       description='API wrapper and imagery access for the GBDX Platform',
       classifiers=[],
       keywords='',
