f5109a35e45a1eeddcea5fe939fed9c7aa1886da
drwelby <marc.pfister@digitalglobe.com>
2019-12-19 11:03:05 -0800

    Switch BLM from TMSImage to Browse

--- gbdxtools/images/mixins/geo.py
+++ gbdxtools/images/mixins/geo.py
@@ -9,6 +9,7 @@ except ImportError:
 import mercantile
 from shapely.geometry import box, shape, mapping, asShape
 from gbdxtools.deprecate import deprecation
+from gbdxtools.images.browse_image import BrowseImage
 
 import numpy as np
 try:
@@ -16,6 +17,7 @@ try:
     has_pyplot = True
 except:
     has_pyplot = False
+import warnings
 
 
 class PlotMixin(object):
@@ -55,9 +57,6 @@ class PlotMixin(object):
         else:
             raise KeyError('Unknown histogram parameter, use "equalize", "match", "minmax", or "ignore"')
 
-    def base_layer_match(self, *args, **kwargs):
-        deprecation('The use of base_layer_match has been deprecated. Please use image.rgb(histogram = "match")')
-
     def histogram_equalize(self, use_bands, **kwargs):
         ''' Equalize and the histogram and normalize value range
             Equalization is on all three bands, not per-band'''
@@ -78,21 +77,16 @@ class PlotMixin(object):
         else:
             return image_equalized
 
-    def histogram_match(self, use_bands, blm_source=None, **kwargs):
+    def histogram_match(self, use_bands, blm_source='browse', **kwargs):
         ''' Match the histogram to existing imagery '''
+        warnings.warn('Histogram matching has changed due to the Maps API deprecation, see https://github.com/DigitalGlobe/gbdxtools/issues/778')
         assert has_rio, "To match image histograms please install rio_hist"
         data = self._read(self[use_bands,...], **kwargs)
         data = np.rollaxis(data.astype(np.float32), 0, 3)
         if 0 in data:
             data = np.ma.masked_values(data, 0)
         bounds = self._reproject(box(*self.bounds), from_proj=self.proj, to_proj="EPSG:4326").bounds
-        if blm_source == 'browse':
-            from gbdxtools.images.browse_image import BrowseImage
-            ref = BrowseImage(self.cat_id, bbox=bounds).read()
-        else:
-            from gbdxtools.images.tms_image import TmsImage
-            tms = TmsImage(zoom=self._calc_tms_zoom(self.affine[0]), bbox=bounds, **kwargs)
-            ref = np.rollaxis(tms.read(), 0, 3)
+        ref = BrowseImage(self.cat_id, bbox=bounds).read()
         out = np.dstack([rio_match(data[:,:,idx], ref[:,:,idx].astype(np.double)/255.0)
                         for idx in range(data.shape[-1])])
         if 'stretch' in kwargs or 'gamma' in kwargs:
@@ -162,7 +156,7 @@ class PlotMixin(object):
         Histogram options:
             * 'equalize': performs histogram equalization on the image.
             * 'minmax': stretch the pixel range to the minimum and maximum input pixel values. Equivalent to stretch=[0,100].
-            * 'match': match the histogram to the Maps API imagery. Pass the additional keyword blm_source='browse' to match to the Browse Service (image thumbnail) instead.
+            * 'match': match the histogram to the Browse Service (image thumbnail), see https://github.com/DigitalGlobe/gbdxtools/issues/778
             * 'ignore': Skip dynamic range adjustment, in the event the image is already correctly balanced and the values are in the correct range.
             
         Gamma values greater than 1 will brighten the image midtones, values less than 1 will darken the midtones.
