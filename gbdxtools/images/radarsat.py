a9afed55b226b3ce49ba7c233b043a5aab2152f8
Mohammad Chaudry <mohammad.chaudry@digitalglobe.com>
2020-01-15 11:59:35 -0500

    Replace RadarsatRead operator with Radarsat2ProductRead

--- gbdxtools/images/radarsat.py
+++ gbdxtools/images/radarsat.py
@@ -8,13 +8,19 @@ rda = RDA()
 
 
 class RadarsatDriver(RDADaskImageDriver):
-    image_option_support = ["proj"]
-    __image_option_defaults__ = {"proj": "EPSG:4326"}
+    image_option_support = ["calibration", "proj"]
+    __image_option_defaults__ = {"calibration": "uncalibrated", "proj": "EPSG:4326"}
 
 
 class Radarsat(RDABaseImage):
     """
-      Dask based access to sentinel2 images backed by rda Graphs.
+      Dask based access to Radarsat images backed by rda Graphs.
+
+     Args:
+        catID (str): The Radarsat record from the Radarsat Catalog Item
+        calibration (str): Data calibration. Options are 'uncalibrated' (default), 'beta0', 'sigma0' or 'gamma'.
+        proj (str): EPSG code for the resulting image.
+        gsd (str): Resolution for the resulting image.
     """
     __Driver__ = RadarsatDriver
 
@@ -22,15 +28,19 @@ class Radarsat(RDABaseImage):
     def _id(self):
         return self.__rda_id__
 
+    @property
+    def polarization(self):
+        return self.options["calibration"]
+
     @classmethod
-    def _build_graph(cls, record, proj=None, gsd=None):
+    def _build_graph(cls, record, calibration="uncalibrated", proj=None, gsd=None):
         try:
             prefix = record['properties']['attributes']['bucketPrefix']
             bucket = record['properties']['attributes']['bucketName']
         except Exception:
             raise AttributeError(
                 "Radarsat Record missing bucketPrefix and bucketName. Most likely this is permissions issue.")
-        radarsat = rda.RadarsatTemplate(path=os.path.join(bucket, prefix), nodeId="RadarsatRead")
+        radarsat = rda.RadarsatTemplate(path=bucket+'/'+prefix, radarsatCalibration=calibration, nodeId="Radarsat2ProductRead")
         if proj is not None:
             params = ortho_params(proj, gsd=gsd)
             radarsat(nodeId="Orthorectify", **params)
