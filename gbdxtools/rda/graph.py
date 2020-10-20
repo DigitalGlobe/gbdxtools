6ad201192b77afb905dccc6678d92139274fb929
drwelby <marc.pfister@digitalglobe.com>
2020-10-20 10:24:57 -0700

    take out cache ttl, not needed

--- gbdxtools/rda/graph.py
+++ gbdxtools/rda/graph.py
@@ -3,42 +3,13 @@ import json
 import time
 from gbdxtools.rda.error import NotFound, BadRequest
 from urllib.parse import urlencode
-import functools
+from functools import lru_cache
 
 VIRTUAL_RDA_URL = os.environ.get("VIRTUAL_RDA_URL", "https://rda.geobigdata.io/v1")
 
 
-# from https://stackoverflow.com/questions/31771286/python-in-memory-cache-with-time-to-live
-
-# We don't want to overload RDA with redundant requests, but because the metadata endpoint is used
-# to see if a strip is available we have to consider that users might poll the endpoint. Since
-# RDA has a 1 minute retry time limit we'll cache for a minute
-
-def time_cache(max_age, maxsize=128, typed=False):
-    """Least-recently-used cache decorator with time-based cache invalidation.
-
-    Args:
-        max_age: Time to live for cached results (in seconds).
-        maxsize: Maximum cache size (see `functools.lru_cache`).
-        typed: Cache on distinct input types (see `functools.lru_cache`).
-    """
-    def _decorator(fn):
-        @functools.lru_cache(maxsize=maxsize, typed=typed)
-        def _new(*args, __time_salt, **kwargs):
-            return fn(*args, **kwargs)
-
-        @functools.wraps(fn)
-        def _wrapped(*args, **kwargs):
-            return _new(*args, **kwargs, __time_salt=int(time.time() / max_age))
-
-        return _wrapped
-
-    return _decorator
-
-
-@time_cache(60)
+@lru_cache()
 def req_with_retries(conn, url, retries=5):
-    print(url)
     for i in range(retries):
         try:
             res = conn.get(url)
